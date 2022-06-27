86314266e7642fbb1590fe0fa95ea3d1b48f94d1
arpi5191 <69826982+arpi5191@users.noreply.github.com>
2022-06-27 14:13:29 -0700

    Update curl-test.sh

--- app/curl-test.sh
+++ app/curl-test.sh
@@ -1,3 +1,3 @@
-CURL --request POST http://localhost:5000/api/timeline_post -d 'name=Arpitha&email=arpitha.rao@mlh.io&content=JustAdded Database to my portfolio site!'
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Arpitha&email=arpitha.rao@mlh.io&content=JustAdded Database to my portfolio site!'
 
-CURL --request GET http://localhost:5000/api/timeline_post
+curl --request GET http://localhost:5000/api/timeline_post
