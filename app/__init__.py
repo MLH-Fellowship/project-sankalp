0980d4ea8774fe13efa5e4ea8271cad0ec6166e7
arpi5191 <69826982+arpi5191@users.noreply.github.com>
2022-07-03 01:06:14 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -78,3 +78,14 @@ TimelinePost.select().order_by(TimelinePost.created_at.desc())
 @app.route('/Timeline')
 def timeline():
     return render_template('timeline.html', title='Timeline')
+
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=true)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        post=3306
+    )
