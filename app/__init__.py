5d25ce15e8612ee0f037d5d2744c3d161f237a6f
arpi5191 <69826982+arpi5191@users.noreply.github.com>
2022-07-03 02:56:17 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -75,6 +75,25 @@ TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
 
+# @app.route('/api/movie_post', methods=['POST'])
+# def post_movie_post():
+#     name = request.form['name']
+#     email = request.form['email']
+#     content = request.form['content']
+#     timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+#     return model_to_dict(timeline_post)
+
+# @app.route('/api/movie_post', methods=['GET'])
+# def get_movie_post():
+#     return {
+#         'movies_posts': [
+#             model_to_dict(p)
+#             for p in
+# TimelinePost.select().order_by(TimelinePost.created_at.desc())
+#         ]
+#     }
+
 @app.route('/Timeline')
 def timeline():
     return render_template('timeline.html', title='Timeline')
