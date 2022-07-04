1486252eb4a8ad1edaa22c7da49879ba0cbb280a
arpi5191 <69826982+arpi5191@users.noreply.github.com>
2022-07-04 12:21:51 -0700

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,7 @@
 # tests.py db
 import unittest
 from peewee import *
-# from app import TimelinePost, get_timeline_posts
-from app import TimelinePost
-
+from app import TimelinePost, get_time_line_post
 
 MODELS=[TimelinePost]
 #use an in-memory SQLite for tests.
@@ -34,12 +32,11 @@ class TestTimelinePost(unittest.TestCase):
         assert first_post.id == 1
         second_post=TimelinePost.create(name='Jane Doe',email='jame@example.com',content='Hello world,I\'mJane!')
         assert second_post.id == 2
-        #TODO:Get timeline posts and assert that they are correct
 
         #Check if length of list within dictionary is 2 as 2 posts were created previously. (Get function returns dictionary with list of posts)
-        # response = get_timeline_posts() #We call get posts function
-        # assert len(response["timeline_posts"]) == 2
+        response = get_time_line_post() #We call get posts function
+        assert len(response["timeline_posts"]) == 2
 
         #Check if posts in list are the same as the ones inserted 2 is the first in the list and 1 is the second
-        # assert response["timeline_posts"][0]["name"] =='Jane Doe' #Jane Doe
-        # assert response["timeline_posts"][1]["name"] =='John Doe' #John Doe
+        assert response["timeline_posts"][0]["name"] =='Jane Doe' #Jane Doe
+        assert response["timeline_posts"][1]["name"] =='John Doe' #John Doe
